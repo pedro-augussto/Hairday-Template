e5361e48ad743b5a7fe827fb75a843f14ccc335d
Pedro Augusto <pa.severosilva@gmail.com>
2024-09-23 20:55:58 -0300

    Renderizando os horarios disponiveis

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 
 import { oppeningHours } from "../../utils/oppening-hours.js";
 
+const hours = document.getElementById("hours");
 
 export function hoursLoad({date}){
     const oppening = oppeningHours.map((hour) => {
@@ -18,5 +19,17 @@ export function hoursLoad({date}){
             available: isHourPast,
         }
 
+    })
+    
+    //Renderizar os horarios
+    
+    oppening.forEach(({hour , available}) => {
+        const li = document.createElement("li");
+        
+        li.classList.add("hour");
+        li.classList.add(available ? "hour-available" : "hour-unavailable");
+        
+        li.textContent = hour;
+        hours.append(li);
     })
 }
