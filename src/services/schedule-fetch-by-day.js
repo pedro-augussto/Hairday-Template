7374d4e6d5e86bf06e0efb217940dd214cdf24c9
Pedro Augusto <pa.severosilva@gmail.com>
2024-09-24 14:11:27 -0300

    Criando o filtro dos agendamentos

---
+++ src/services/schedule-fetch-by-day.js
@@ -0,0 +1,23 @@
+import dayjs from "dayjs";
+import { apiConfig } from "./api-config";
+
+export async function scheduleFetchByDay ({ date }) {
+    try {
+        //Fazendo a requisição
+        const response = await fetch (`${apiConfig.baseURL}/schedules`);
+
+        //Converte para JSON
+        const data = await response.json();
+
+        //Filtra pos agendamentos pelos dia selecionados
+        const dailySchedules = data.filter((schedule) => 
+            dayjs(date).isSame(schedule.when, "date")
+        );
+
+        return dailySchedules;
+    } catch (error) {
+
+        alert("Não foi possivel buscar os agendamentos do dia selecionado.");
+        console.log(error);
+    }
+}
