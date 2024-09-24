cd2d837a1c80b5e82ff86f20d91a6cf53f575ba7
Pedro Augusto <pa.severosilva@gmail.com>
2024-09-23 21:20:35 -0300

    Enviando os horarios selecionados

--- src/modules/form/submite.js
+++ src/modules/form/submite.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 
 const form = document.querySelector("form");
+const clientName = document.getElementById("client");
 const selectedDate = document.getElementById("date");
 
 //Data atual para o input
@@ -17,5 +18,38 @@ form.onsubmit = (event) => {
     //Prevenindo comportamento de recarrega a pagina
     event.preventDefault();
 
-    console.log("Enviado")
+    try {
+        //Recuperando o nome do cliente
+        const name = clientName.value.trim();
+
+        if(!name){
+            return alert("Informe o nome do cliente.");
+        }
+
+        //Recuperando a hora selecionada
+        const hourSelected = document.querySelector(".hour-selected");
+
+        if(!hourSelected){
+            return alert("Selecione a hora.");
+        }
+
+        //Recuperando somente a hora
+        const [hour] = hourSelected.innerText.split(":");
+
+        //Insere a hora com a data
+        const when = dayjs(selectedDate.value).add(hour, "hour");
+
+        //Gerar um id
+        const id = new Date().getTime();
+
+        console.log({
+            id,
+            name,
+            when,
+        })
+
+    } catch (error) {
+        alert("Não foi possivel agendar o horario.");
+        console.log(error);
+    }
 }
